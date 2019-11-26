d436b2b0803ffe28c0b7a82ad0220759e7e640f0
HoangViet <viet14042000gmail.com>
2019-11-26 11:19:08 +0700

    add write2file function

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@
 using namespace std;
 using json = nlohmann::json;
 json j;
+json out;
 struct action
 {
     int id;
@@ -44,17 +45,17 @@ struct match
 {
     int width;
     int height;
-    int **points;
+    vector<vector<int>>points;
     int startedAtUnixTime;
     int turn;
-    int **tiled;
+    vector<vector<int>>tiled;
     team tA, tB;
-    action A, B;
+    vector<action> act;
 };
 match now;
+fstream ifs;
 void readMap(int i)
 {
-    fstream ifs;
     ifs.open("fieldInfo_turn"+to_string(i)+".json",ios::in);
     ifs>>j;
     now.width=j["width"];
@@ -62,7 +63,6 @@ void readMap(int i)
     now.startedAtUnixTime=j["startedAtUnixTime"];
     now.turn=j["turn"];
     int ind=0;
-    //j["teams"].get_to(now.te);
     string s=j["teams"].dump(2);
     now.tA.teamID=j["teams"][0]["teamID"];
     now.tA.tilePoint=j["teams"][0]["tilePoint"];
@@ -88,21 +88,62 @@ void readMap(int i)
         now.tA.B.push_back(tmp);
         now.tB.B.push_back(tmp);
     }
-   // cout<<s;
-    for (json::iterator it = j["teams"].begin(); it != j["teams"].end(); ++it) {
-        //std::cout << it["agents"] << '\n';
- //       cout<<*it<<endl;
-
-        //string s=*it;
-        //cout<<s<<endl;
+    FOR(i,0,now.height)
+    {
+        vector<int>tmp;
+        j["points"][i].get_to(tmp);
+        now.points.push_back(tmp);
     }
-
-    //now
-//    cout<<now.tA.teamID;
+    FOR(i,0,now.height)
+    {
+        vector<int>tmp;
+        j["tiled"][i].get_to(tmp);
+        now.tiled.push_back(tmp);
+    }
+    FOR(i,0,j["actions"].size())
+    {
+        action tmp;
+        tmp.id=j["actions"][i]["agentID"];
+        tmp.type=j["actions"][i]["type"];
+        tmp.dx=j["actions"][i]["dx"];
+        tmp.dy=j["actions"][i]["dy"];
+        tmp.turn=j["actions"][i]["turn"];
+        tmp.apply=j["actions"][i]["apply"];
+        now.act.push_back(tmp);
+    }
+    ifs.close();
+}
+void writeOutput(vector<action> decide)
+{
+    out=
+    {
+        {"actions",{}},
+    };
+    FOR(i,0,decide.size())
+    {
+        out["actions"].push_back("{ \"agentID\": 15, \"type\": 15,\"dx\": 15,\"dy\": 15 }"_json);
+        out["actions"][i]["agentID"]=decide[i].id;
+        out["actions"][i]["type"]=decide[i].type;
+        out["actions"][i]["dx"]=decide[i].dx;
+        out["actions"][i]["dy"]=decide[i].dy;
+    }
+    ifs.open("decide.json",ios::out);
+    ifs<<std::setw(4)<<out;
+    ifs.close();
 }
 int main()
 {
     readMap(0);
-
+    vector<action> decide;
+    action tmp;
+    tmp.apply=5;
+    tmp.dx=1;
+    tmp.dy=2;
+    tmp.id=3;
+    tmp.turn=4;
+    tmp.type="fdas";
+    decide.push_back(tmp);
+    decide.push_back(tmp);
+    writeOutput(decide);
     return 0;
 }
